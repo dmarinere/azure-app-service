95a68e915c3561c88bdee8fba355b0e9d2e4e3e6
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2022-05-14 01:08:34 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+  
+  
+if __name__ == '__main__':
+   app.run()
