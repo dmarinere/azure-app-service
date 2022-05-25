d5731596166551c60093abba59ed212c042193e0
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2022-05-25 08:03:47 +0100

    Update app.py
    Added code that return date time

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+   date = str(datetime.now())
+   return render_template('index.html', date)
   
   
 if __name__ == '__main__':
